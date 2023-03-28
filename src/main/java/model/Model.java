6b0bb5aff839894d2af7db7246321253c160b25e
Gabriel Bor <gabrielbor@hotmail.co.uk>
2023-03-28 15:51:46 +0100

    init

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -36,7 +36,6 @@ public class Model
   }
 
   // Manage the lists (add, get, delete, edit):
-  // Add a new list directory in the data directory
   public void addList(String listName) throws IOException {
     if (listName == null || listName.isEmpty()) {
       return;
@@ -49,7 +48,6 @@ public class Model
     allLists.add(new AList(listName));
     ListFilesEditor.addListInData(listName);
   }
-  // Get the names of all the lists
   public String[] getListNames() {
     ArrayList<String> listNames = new ArrayList<>();
     for (AList list : allLists) {
@@ -58,12 +56,10 @@ public class Model
     String[] names = new String[listNames.size()];
     return listNames.toArray(names);
   }
-  // Delete list directory
   public void deleteList(String listName) {
     this.allLists.remove(getList(listName));
     ListFilesEditor.deleteListInData(listName);
   }
-  // Edit the name of the list
   public void changeListName(String listName, String newListName) throws IOException {
     for (AList list : allLists) {
       if (newListName.equals(list.getName())) {
@@ -86,14 +82,13 @@ public class Model
 
     ListFilesEditor.editListNameInData(listName, newListName);
   }
+
   // Manage the list items (add, get, delete, edit):
-  // Add items to a list
   public void addListItem(String listName, String itemName) throws IOException {
     AList list = getList(listName);
     list.addListItem(itemName);
     ListFilesEditor.addListItemInData(listName, itemName);
   }
-  // Get items from a list
   public String[] getListItems(String listName) {
     ArrayList<String> itemNames = new ArrayList<>();
     for (Item item : getList(listName).getItems()) {
@@ -102,72 +97,45 @@ public class Model
     String[] names = new String[itemNames.size()];
     return itemNames.toArray(names);
   }
-  // Delete item from a list
   public void deleteListItem(String listName, String itemName){
     AList list = getList(listName);
     list.getItems().remove(list.getItem(itemName));
     ListFilesEditor.deleteListItem(listName, itemName);
   }
 
-  // Editing the items in the list (set, get) :
-  // Edit item's name
+  // Editing the items in the list:
   public void changeItemName(String listName, String itemName, String newName) {
     AList list = getList(listName);
     Item item = list.getItem(itemName);
     item.changeItemName(newName);
   }
-  // Set and get item text
   public void setItemText(String listName, String itemName, String text) throws IOException {
     AList list = getList(listName);
     Item item = list.getItem(itemName);
     item.setItemText(text);
   }
-  public String getItemText(String listName, String itemName) {
-    AList list = getList(listName);
-    Item item = list.getItem(itemName);
-    return item.getText();
-  }
-  // Set and get item URL
   public void setItemURL(String listName, String itemName, String URL) throws IOException {
     AList list = getList(listName);
     Item item = list.getItem(itemName);
     item.setItemURL(URL);
   }
-  public String getItemURL(String listName, String itemName) {
-    AList list = getList(listName);
-    Item item = list.getItem(itemName);
-    return item.getUrl();
-  }
-  // Set and get item Image
   public void setItemImage(String listName, String itemName, Part filePart) throws IOException {
     AList list = getList(listName);
     Item item = list.getItem(itemName);
     item.setItemImage(filePart);
-  }
-  public String getItemImage(String listName, String itemName) {
-    AList list = getList(listName);
-    Item item = list.getItem(itemName);
-    return item.getImage();
   }
   public void deleteItemImage(String listName, String itemName) {
     AList list = getList(listName);
     Item item = list.getItem(itemName);
     item.deleteItemImage();
   }
-  // Set, get and update links to other list
   public void setItemListLink(String listName, String itemName, String listLink) throws IOException {
     AList list = getList(listName);
     Item item = list.getItem(itemName);
     item.setItemListLink(listLink);
   }
 
-  public String getItemListLink(String listName, String itemName) {
-    AList list = getList(listName);
-    Item item = list.getItem(itemName);
-    return item.getListLink();
-  }
-
-
+  //Searching for items and lists:
   public List<Item> searchForItem(String searchQuery) {
     if (getListNames() == null || getListNames().length == 0) {
       return Collections.emptyList();
